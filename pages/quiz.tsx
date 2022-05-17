1b127a690368800d4427260d5182bccdc6e45eb3
Leer0r <avedd72@gmail.com>
2022-05-17 16:53:51 +0200

    push changes

---
+++ pages/quiz.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import styles from "../styles/Quiz.module.scss"
+
+const Quiz: NextPage = () => {
+    return(
+        <div className="">
+            coucou
+        </div>
+    )
+}
+
+export default Quiz;
